7d36793ddb9cf6bb0f1f17e5d4769230453267fc
山然 <100954636@qq.com>
2024-07-25 19:14:39 +0800

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,18 @@
-// const uni = require('@uni-helper/eslint-config')
-// const unocss = require('@unocss/eslint-plugin')
+const uni = require('@uni-helper/eslint-config')
+const unocss = require('@unocss/eslint-plugin')
 
-// module.exports = uni(
-//   {
-//     overrides: {
-//       uni: {
-//         'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-//       },
-//     },
-//   },
-//   unocss.configs.flat,
-//   {
-//     rules: {
-//       'no-console': 'off',
-//     },
-//   },
-// )
+module.exports = uni(
+  {
+    overrides: {
+      uni: {
+        'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+      },
+    },
+  },
+  unocss.configs.flat,
+  {
+    rules: {
+      'no-console': 'off',
+    },
+  },
+)
